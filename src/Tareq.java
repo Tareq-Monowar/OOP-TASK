dae191f39c0a5adb3120bda92bcb686d4ab8dbaf
Tareq-Monowar <tareq31072003@gamil.com>
2024-12-15 10:27:19 +0600

    volume of a ball

--- src/Tareq.java
+++ src/Tareq.java
@@ -4,9 +4,10 @@ import java.util.Scanner;
 public class Tareq {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double a= s.nextInt();
-        double b= s.nextInt();
-        double area= 0.5*a*b;
-        System.out.println(area);
+        double r= s.nextInt();
+        //double b= s.nextInt();
+        double pi=3.1416;
+        double volume= 1.3333333*pi*r*r*r;
+        System.out.println(volume);
     }
 }
