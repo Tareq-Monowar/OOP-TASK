43b4ef6a0f062b40e90924e3f9288549b1a84e6d
Tareq-Monowar <tareq31072003@gamil.com>
2024-12-15 10:13:36 +0600

    area of triangle

---
+++ src/Tareq.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Tareq {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double a= s.nextInt();
+        double b= s.nextInt();
+        double area= 0.5*a*b;
+        System.out.println(area);
+    }
+}
