879dc7c85159b6509a6877b71c384705548afdfa
Tareq-Monowar <tareq31072003@gamil.com>
2025-01-18 16:37:33 +0600

    triangle

--- Practice_Problem_01/task01.java
+++ Practice_Problem_01/task01.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class task01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        double base=s.nextDouble();
-        double hight=s.nextDouble();
-        System.out.println("Area of a triangle:");
-        System.out.println(0.5*base*hight);
+        double radious=s.nextDouble();
+        //double hight=s.nextDouble();
+        System.out.println("Area of a Circle:");
+        System.out.println(3.1416*radious*radious);
     }
 }
