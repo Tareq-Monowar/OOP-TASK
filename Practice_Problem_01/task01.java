0d77f84fc6c7899c00a7793787c539bd67e132ec
Tareq-Monowar <tareq31072003@gamil.com>
2025-01-18 16:40:34 +0600

    circle

--- Practice_Problem_01/task01.java
+++ Practice_Problem_01/task01.java
@@ -6,7 +6,6 @@ public class task01 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         double radious=s.nextDouble();
-        //double hight=s.nextDouble();
         System.out.println("Area of a Circle:");
         System.out.println(3.1416*radious*radious);
     }
