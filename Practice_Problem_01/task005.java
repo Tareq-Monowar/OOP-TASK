159579f4f725e4195823254d8d9e169fd113939a
Tareq-Monowar <tareq31072003@gamil.com>
2025-01-21 22:43:32 +0600

    loop+nested loop

--- Practice_Problem_01/task005.java
+++ Practice_Problem_01/task005.java
@@ -10,11 +10,24 @@ public class task005 {
         printSumFor(Number);
         System.out.println("While loop");
         printSumWhile(Number);
+
+        for(int i=1;i<Number;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.print("\n");
+        }
+
+        for(int i=1;i<Number;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.print("\n");
+        }
     }
 
 
     private static void printSumFor(int a) {
-        // Print and calculate sum of even numbers
         int sumEven= 0, sumOdd= 0;
         System.out.print("Even series: ");
         for (int i = 2; i <= a; i += 2) {
@@ -23,7 +36,6 @@ public class task005 {
         }
         System.out.println("\nSum of even series: " + sumEven);
 
-        // Print and calculate sum of odd numbers
         System.out.print("Odd series: ");
         for (int i = 1; i <= a; i += 2) {
             System.out.print(i + " ");
@@ -33,7 +45,6 @@ public class task005 {
     }
 
     private static void printSumWhile(int b) {
-        // Print and calculate sum of even numbers
         int i = 2, sumEven = 0, sumOdd = 0;
         System.out.print("Even series: ");
         while (i <= b) {
@@ -43,7 +54,6 @@ public class task005 {
         }
         System.out.println("\nSum of even series: " + sumEven);
 
-        // Print and calculate sum of odd numbers
         i = 1;
         System.out.print("Odd series: ");
         while (i <= b) {
